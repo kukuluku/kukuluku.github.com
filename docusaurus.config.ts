4dc175a4998805d66f38cf9caec2bba66ad8aebc
kyungkeunkang <kk.kang@me.com>
2025-01-31 14:36:01 +0900

    feat: add naver search adv

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,12 +5,12 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'kukuluku wiki',
+  tagline: 'kukuluku dev wiki',
   favicon: 'img/github-icon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://kukuluku.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
